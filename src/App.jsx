a23546e4411d6c24084e843aafcbaba1e4f2a15b
Bijay <sthabeejay2060@gmail.com>
2024-03-18 11:57:31 +0545

    CMS Frontend

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+     <h1 style={{color:"red"}}>Hello World!</h1>
+      <p>Clicked times</p>
+      <h2>Age: {21+2}</h2>
+    </div>
+  )
+}
+
+export default App
